91d1446fdca89390924626573f1693da1d758502
Idenyi Gabriel <idenyigabriel@gmail.com>
2023-02-20 07:36:34 +0100

    '#Fix - Request validator schema"
    - added pattern to schema property field name, to prevent empty todo name from being created

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,16 +4,12 @@ import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../helpers/todos'
-import * as createError from 'http-errors'
+
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
-
-    if (!newTodo.name) {
-      throw new createError.BadRequest('Invalid request body')
-    }
 
     const userId = getUserId(event)
     const item = await createTodo(userId, newTodo)
